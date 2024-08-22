e036e6353f0e56a73636023150015a6f5594730c
VUDUMULA NAGA SAI RAHUL <110412514+Rahul21sai@users.noreply.github.com>
2024-08-22 23:52:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Alien Dictionary/alien-dictionary.java
+++ Difficulty: Hard/Alien Dictionary/alien-dictionary.java
@@ -2,158 +2,148 @@
 /*package whatever //do not write package name here */
 
 import java.io.*;
-import java.util.*;
 import java.math.*;
+import java.util.*;
 
 class GFG {
-	public static void main (String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int t = Integer.parseInt(sc.next());
-		while(t-- > 0)
-		{
-		    int n = Integer.parseInt(sc.next());
-		    int k = Integer.parseInt(sc.next());
-		    
-		    String[] words = new String[n];
-		    
-		    for(int i=0;i<n;i++)
-		    {
-		        words[i] = sc.next();
-		    }
-		    
-		    Solution ob = new Solution();
-		  //  System.out.println(T.findOrder(words,k));
-		    String order = ob.findOrder(words,n,k);
-		    if(order.length() == 0){
-		        System.out.println(0);
-		        continue;
-		    }
-		    if(order.length() != k){
-		        System.out.println("INCOMPLETE");
-		        return;
-		    }
-		    String temp[] = new String[n];
-		    for(int i=0;i<n;i++)
-		        temp[i] = words[i];
-		    
-		    Arrays.sort(temp, new Comparator<String>(){
-		    
-		      @Override
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = Integer.parseInt(sc.next());
+        while (t-- > 0) {
+            int n = Integer.parseInt(sc.next());
+            int k = Integer.parseInt(sc.next());
+
+            String[] words = new String[n];
+
+            for (int i = 0; i < n; i++) {
+                words[i] = sc.next();
+            }
+
+            Solution ob = new Solution();
+            //  System.out.println(T.findOrder(words,k));
+            String order = ob.findOrder(words, n, k);
+            if (order.length() == 0) {
+                System.out.println(0);
+                continue;
+            }
+            if (order.length() != k) {
+                System.out.println("INCOMPLETE");
+                return;
+            }
+            String temp[] = new String[n];
+            for (int i = 0; i < n; i++) temp[i] = words[i];
+
+            Arrays.sort(temp, new Comparator<String>() {
+                @Override
                 public int compare(String a, String b) {
                     int index1 = 0;
                     int index2 = 0;
-                    for(int i = 0; i < Math.min(a.length(), b.length()) 
-                                        && index1 == index2; i++) {
+                    for (int i = 0;
+                         i < Math.min(a.length(), b.length()) && index1 == index2;
+                         i++) {
                         index1 = order.indexOf(a.charAt(i));
                         index2 = order.indexOf(b.charAt(i));
                     }
-                
-                    if(index1 == index2 && a.length() != b.length()) 
-                    {
-                        if(a.length() < b.length())
+
+                    if (index1 == index2 && a.length() != b.length()) {
+                        if (a.length() < b.length())
                             return -1;
                         else
                             return 1;
                     }
-                
-                    if(index1 < index2)
+
+                    if (index1 < index2)
                         return -1;
                     else
                         return 1;
-                        
                 }
-		    });
-		    
-		    int flag = 1;
-		    for(int i=0;i<n;i++)
-		    {
-		        if(!words[i].equals(temp[i]))
-	            {
-	                flag = 0;
-	                break;
-	            }
-		    }
-		    
-		    System.out.println(flag);
-		}
-	}
-	
+            });
+
+            int flag = 1;
+            for (int i = 0; i < n; i++) {
+                if (!words[i].equals(temp[i])) {
+                    flag = 0;
+                    break;
+                }
+            }
+
+            System.out.println(flag);
+        }
+    }
 }
 
 // } Driver Code Ends
 
 
+// User function Template for Java
+
+
+
 //User function Template for Java
 
-class Solution
-{
-    public String findOrder(String [] dict, int N, int K)
-    {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        
-        for(int i = 0; i < K;i++){
+
+
+//User function Template for Java
+
+class Solution {
+    public String findOrder(String[] dict, int n, int k) {
+        // Write your code here
+        // Step 1: Create an adjacency list for the graph
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < k; i++) {
             adj.add(new ArrayList<>());
         }
         
-        for(int i = 0; i< N-1;i++){
-            String s1 = dict[i];
-            String s2 = dict[i+1];
-            int len = Math.min(s1.length(), s2.length());
-            for(int ptr =0 ; ptr<len;ptr++){
-                if(s1.charAt(ptr) != s2.charAt(ptr)){
-                    adj.get(s1.charAt(ptr) - 'a').add(s2.charAt(ptr)-'a');
-                    break;
+        // Step 2: Build the graph by comparing adjacent words
+        for (int i = 0; i < n - 1; i++) {
+            String word1 = dict[i];
+            String word2 = dict[i + 1];
+            int len = Math.min(word1.length(), word2.length());
+            for (int j = 0; j < len; j++) {
+                if (word1.charAt(j) != word2.charAt(j)) {
+                    adj.get(word1.charAt(j) - 'a').add(word2.charAt(j) - 'a');
+                    break; // only the first different character matters
                 }
             }
         }
         
-        List<Integer> topo  = topo(K,adj);
-        String ans= "";
-        for(int it : topo){
-            ans = ans + (char)(it + (int)('a'));
-        }
-        
-        return ans;
-        
-        
-    }
-    
-    private List<Integer> topo(int v, ArrayList<ArrayList<Integer>> adj){
-        
-        int[] ind = new int[v];
-        
-        for(int i = 0; i< v;i++){
-            for(int it: adj.get(i)){
-                ind[it]++;
+        // Step 3: Perform topological sort (Kahn's algorithm)
+        int[] inDegree = new int[k];
+        for (int i = 0; i < k; i++) {
+            for (int v : adj.get(i)) {
+                inDegree[v]++;
             }
         }
         
         Queue<Integer> q = new LinkedList<>();
+        StringBuilder order = new StringBuilder();
         
-        List<Integer> topo = new ArrayList<>();
-        
-        for(int i = 0; i< v;i++){
-            if(ind[i] == 0){
-                q.add(i);
+        // Add nodes with 0 in-degree to the queue
+        for (int i = 0; i < k; i++) {
+            if (inDegree[i] == 0) {
+                q.offer(i);
             }
         }
         
-        while(!q.isEmpty()){
-            int node = q.peek();
-            q.remove();
-            topo.add(node);
+        // BFS (Kahn's Algorithm)
+        while (!q.isEmpty()) {
+            int u = q.poll();
+            order.append((char)(u + 'a'));
             
-            for(int it : adj.get(node)){
-                ind[it]--;
-                if(ind[it] == 0){
-                    q.add(it);
+            for (int v : adj.get(u)) {
+                inDegree[v]--;
+                if (inDegree[v] == 0) {
+                    q.offer(v);
                 }
             }
         }
         
-        return topo;
+        // If the order has the same number of characters as k, it's valid
+        if (order.length() == k) {
+            return order.toString();
+        }
         
+        return "";
     }
-    
 }
